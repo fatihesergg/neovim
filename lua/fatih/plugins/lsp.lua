4593943b4fb7b836cb8735d5c3e44b4e7b26456a
Fatih Eser <fatihesergg@gmail.com>
2024-08-27 14:10:45 +0300

    update

--- lua/fatih/plugins/lsp.lua
+++ lua/fatih/plugins/lsp.lua
@@ -9,41 +9,64 @@ return
             "hrsh7th/cmp-buffer",
             "hrsh7th/cmp-path",
             "hrsh7th/cmp-cmdline",
+            "j-hui/fidget.nvim",
+            "WhoIsSethDaniel/mason-tool-installer.nvim",
         },
-
         config = function ()
+            local telescope_builtin = require("telescope.builtin")
             local cmp = require("cmp")
             local cmp_lsp = require("cmp_nvim_lsp")
-            local capabilities = vim.tbl_deep_extend(
-            "force",
-            {},
-            vim.lsp.protocol.make_client_capabilities(),
-            cmp_lsp.default_capabilities())
-            require("mason").setup()
-            require("mason-lspconfig").setup({
-                automatic_installation = true,
-                ensure_installed =  {
-                    "lua_ls",
-                    "omnisharp",
-                    "lemminx",
-                    "tsserver",
-                    "html",
-                    "cssls",
-                    "jsonls",
-                    "yamlls",
-                    "bashls",
-                    "pyright",
-                    "sumneko_lua",
-                },
-                    handlers = {
-                        function(server_name) -- default handler (optional)
-        
-                            require("lspconfig")[server_name].setup {
-                                capabilities = capabilities
-                            }
-                        end,
-                    }
+            
+            -- capabilities
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
+            capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+            
+
+            -- LSP Attach Autocmd
+            vim.api.nvim_create_autocmd("LspAttach", {
+                group = vim.api.nvim_create_augroup("UserLspConfig", {clear=true}),
+                callback = function (event)
+                    local map = function(keys, func, desc, mode)
+                        mode = "n"
+                        vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+                end
+
+                -- Telescope LSP commands
+                map("gD", vim.lsp.buf.declaration, "Go to declaration")
+                map("gd",telescope_builtin.lsp_definitions, "Go to definition")
+                map("gr",telescope_builtin.lsp_references, "Go to references")
+                map("gi", telescope_builtin.lsp_implementations, "Go to implementations")
+                map("K", vim.lsp.buf.hover, "Show documentation")
+                map("<leader>rn", vim.lsp.buf.rename, "Rename")
+                map("ca", vim.lsp.buf.code_action, "Code action")
+             end,
             })
+            local servers =  {
+                lua_ls = {},
+                omnisharp = {},
+                tsserver = {},
+                html = {},
+                cssls = {},
+                jsonls = {},
+                yamlls = {},
+                bashls = {},
+                pyright = {},
+            }
+            require("mason").setup()
+
+            local ensure_installed = vim.tbl_keys(servers or {})
+            require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+            require('mason-lspconfig').setup {
+                handlers = {
+                    function(server_name)
+                        local server = servers[server_name] or {}
+                        server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+                        require('lspconfig')[server_name].setup(server)
+                    end,
+                },
+            }
+
+            require("fidget").setup()
 
             cmp.setup({
                 mapping = cmp.mapping.preset.insert({
--- lua/fatih/remap.lua
+++ lua/fatih/remap.lua
@@ -6,10 +6,7 @@ local autocmd = vim.api.nvim_create_autocmd
 autocmd("LspAttach", {
     callback = function(e)
         local opts =  {buffer=e.buf}
-        vim.keymap.set("n","gd", function() vim.lsp.buf.definition() end,opts)
         vim.keymap.set("n","K",function() vim.lsp.buf.hover() end,opts)
-        vim.keymap.set("n","gr",function() vim.lsp.buf.references() end,opts)
-        vim.keymap.set("n","d(",function() vim.diagnostic.goto_next() end,opts) vim.keymap.set("n","d)",function() vim.diagnostic.goto_prev()end,opts)
         vim.keymap.set("n","<leader>f",function() vim.lsp.buf.format()end,{})
     end
     })
