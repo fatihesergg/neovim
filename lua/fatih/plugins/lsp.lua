0881cc9ecb2fe8ae09e9521f0473cbde13a240fd
fatihesergg <fatihesergg@gmail.com>
2025-01-22 17:27:32 +0300

    add: emmet_ls & set clipboard to unnamedplus

--- lua/fatih/plugins/lsp.lua
+++ lua/fatih/plugins/lsp.lua
@@ -75,8 +75,7 @@ return
                     }
                 }
             },
-            html = {},
-            cssls = {},
+            emmet_ls = {},
             jsonls = {},
             yamlls = {},
             bashls = {},
